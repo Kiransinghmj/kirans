65eabb4e8d98d24931772358ef173607fdffbc5e
Kiransinghmj <123872659+Kiransinghmj@users.noreply.github.com>
2023-01-29 17:27:20 +0530

    Add files via upload

---
+++ r doc.R
@@ -0,0 +1,24 @@
+1+1
+a <- 10
+b<-12
+23->c
+2+3*4
+3^2
+exp(1)
+sqrt(10)
+pi
+2*pi*6378
+vect_x<-c(10,20,30,40)
+vect_y<-c(1,2,3,4)
+vect_x+vect_y
+vect_z<-c(1,2,3)
+vect_x[2]
+df<- data.frame(x=1:3,y=c("a","b","c"))
+Ddf_new<-data.frame(height=c(150,160),weight=c(65,72))
+df[1,1]
+df[1,c(1,2)]
+df[c(1,3),c(1,2)]
+df[c(1,3),1]
+#print value of 1and 3
+df[1,c(1,2)]
+#print a and c
